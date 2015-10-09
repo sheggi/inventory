073434a031b386aec3daca84c0b2228a2f8995a3
sheggi <4.codemaster>
2015-10-09 18:27:40 +0200

    with ng route snipets

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,25 @@
 var app = angular.module("App",["ui.bootstrap"]);
 
+app.factory("businessLogic", ["$http",function($http){
+
+    var getInventory = function(){
+        return $http.get("http://localhost:8000/api/v1/items/");
+
+      //return ["asdfs","asdasfd","adsfsaddddd"];
+    };
+
+
+
+    return {
+        getInventory: getInventory
+    };
+
+}]);
+
+app.controller("listController",["$scope","businessLogic",function($scope,businessLogic){
+    $scope.inventoryList = [];
+    businessLogic.getInventory().then(function(response){
+        console.log(response);
+        $scope.inventoryList = response.data.data;
+    });
+}]);
